4c31afdc6edcaa9a7549b9c6a6f5f56439907925
Aaron Graham <aarograh@umich.edu>
2016-08-17 13:38:17 -0400

    Added name, scatOrder, total, absorption, nufission, fission, chi, and scatter to xsClass.  Added a second contructor to be used by the xs library type that is currently called xsClass2 so MOC_1D will still work.

--- 1D MOC/xsClass.m
+++ 1D MOC/xsClass.m
@@ -3,10 +3,23 @@ classdef xsClass
     %   Detailed explanation goes here
     
     properties
+        name
+        scatOrder
+        total
         transport
+        absorption
+        nufission
+        fission
+        chi
+        scatter
     end
     
     methods
+        function obj = xsClass2( name, order )
+            obj.name = name;
+            obj.scatOrder = order;
+        end
+        
         function obj = xsClass( ngroups )
             obj.transport(1:ngroups,1) = 0.0;
         end
