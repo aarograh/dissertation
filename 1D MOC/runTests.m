26503abaf626366a6390e647948b33f1aa468c97
Aaron Graham <aarograh@umich.edu>
2016-08-26 14:07:13 -0400

    Adding runTests.m.  Automatically runs all tests fount in testLists.txt

---
+++ 1D MOC/runTests.m
@@ -0,0 +1,26 @@
+close all; clear all; clc;
+
+addpath tests
+fid = fopen('testList.txt');
+tests = 0;
+failed = 0;
+EOF = 0;
+
+while ~EOF
+    nextline = fgets(fid);
+    if ischar(nextline)
+        testname = strtrim(strrep(nextline,sprintf('\n'),''));
+        fh = str2func(testname);
+        tests = tests + 1;
+        display(sprintf('Starting test %i: %15s',tests,testname))
+        result = fh();
+        if ~result
+            failed = failed + 1;
+        end
+    else
+        EOF = 1;
+    end
+end
+
+display(sprintf('\n%i of %i tests failed!',failed,tests));
+rmpath tests
