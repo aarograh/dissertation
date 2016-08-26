184f0751152afb5e4ed0fc00ced3f533bc437a2d
Aaron Graham <aarograh@umich.edu>
2016-08-26 12:02:36 -0400

    Adding inputClass container to store input data

---
+++ 1D MOC/inputClass.m
@@ -0,0 +1,33 @@
+classdef inputClass
+    %INPUTCLASS Container class to hold input data
+    %   Contains all data for pin geometry and materials,
+    %   XS Library filename, and ray tracing and meshing
+    %   parameters
+    
+    properties
+        pitch
+        pinmap
+        diag
+        pinmats
+        radii
+        pinmesh
+        npol
+        xsfilename
+        scattype
+        BCond
+        nouters
+        conv_crit
+    end
+    
+    methods
+        function obj = inputClass( )
+            %INPUTCLASS Creates an empty inputClass
+            %   No attributes are initialized.  This
+            %   simply creates the class and leaves
+            %   filling the attributes to the user.
+            
+        end
+    end
+    
+end
+
