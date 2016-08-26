af22c9f34e5b74d4e2cbc0c1e91b405b50d350de
Aaron Graham <aarograh@umich.edu>
2016-08-26 16:00:37 -0400

    Adding additional message to test for the case where the result is right but did not converge as fast as it should have.

--- 1D MOC/tests/test_1GIHM.m
+++ 1D MOC/tests/test_1GIHM.m
@@ -36,6 +36,9 @@ ref = 1.2/0.8;
 if abs(solver.solution.keff(1) - ref) < 1.0e-6 && solver.converged
     display(sprintf('Test Passed! Ref: %0.7f, Test: %0.7f',ref,solver.solution.keff(1)));
     result = 1;
+elseif abs(solver.solution.keff(1) - ref)
+    display(sprintf('Result is correct, but did not converge quickly enough!'));
+    result = 0;
 else
     display(sprintf('Test Failed! Ref: %0.7f, Test: %0.7f',ref,solver.solution.keff(1)));
     result = 0;
--- 1D MOC/tests/test_4GIHM.m
+++ 1D MOC/tests/test_4GIHM.m
@@ -69,6 +69,9 @@ ref = xsnF'*phi;
 if abs(solver.solution.keff(1) - ref) < 1.0e-6 && solver.converged
     display(sprintf('Test Passed! Ref: %0.7f, Test: %0.7f',ref,solver.solution.keff(1)));
     result = 1;
+elseif abs(solver.solution.keff(1) - ref)
+    display(sprintf('Result is correct, but did not converge quickly enough!'));
+    result = 0;
 else
     display(sprintf('Test Failed! Ref: %0.7f, Test: %0.7f',ref,solver.solution.keff(1)));
     result = 0;
