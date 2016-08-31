163305de4a77f25f3ce557ac6e8dc21186a749ad
Aaron Graham <aarograh@umich.edu>
2016-08-31 16:31:06 -0400

    Created test for non-IHM 1G cmfd.  This feature does not work yet.

---
+++ 1D MOC/test_1G_cmfd.m
@@ -0,0 +1,50 @@
+function result = test_1G( verbose )
+%TEST1GIHM Performs a 1-group test with vacuum boundaries
+%   verbose - Flag to enable/disable output
+
+%% General Input Data
+% 1: Fuel Pin
+% 2: Control Pin
+% 3: Guide Tube Pin
+input = inputClass();
+input.pinmap = 1;
+input.pitch = 10.0;
+input.diag = 0; % flat to indicate whether pin moves through narrow (0) or wide (1) water
+% Pin information
+input.pinmats = 4;
+
+input.radii = [ ];
+input.pinmesh = 10;
+% Quadrature
+input.npol = 32;
+% XS Library Info
+input.xsfilename = '1group.xsl';
+input.scattype = 'P0';
+% Boundary Conditions
+input.BCond = ['vacuum';'vacuum'];
+% Convergence
+input.nouters = 1000;
+if exist('verbose','var')
+    input.verbose = verbose;
+else
+    input.verbose = true;
+end
+input.cmfd = true;
+
+%% Test Case
+solver = MOC_1D(input);
+
+%% Test Solution
+ref = 1.4541858;
+if abs(solver.solution.keff(1) - ref) < 1.0e-6 && solver.converged
+    display(sprintf('Test Passed!'));
+    result = 1;
+elseif abs(solver.solution.keff(1) - ref) < 1.0e-6
+    display(sprintf('Result is correct, but did not converge quickly enough!'));
+    result = 0;
+else
+    display(sprintf('Test Failed! Ref: %0.7f, Test: %0.7f',ref,solver.solution.keff(1)));
+    result = 0;
+end
+
+end
