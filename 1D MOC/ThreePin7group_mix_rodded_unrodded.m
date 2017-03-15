91c606e86fa8df7f7cc7ce3f6631c73b85a861eb
Aaron Graham <aarograh@umich.edu>
2017-03-14 23:03:32 -0400

    Adding a 3 pin input for testing sub-ray

---
+++ 1D MOC/ThreePin7group_mix_rodded_unrodded.m
@@ -0,0 +1,41 @@
+close all; clear variables; clc;
+
+%% General Input Data
+% 1: Fuel Pin
+% 2: Control Pin
+% 3: Guide Tube Pin
+input = inputClass();
+input.pitch = 1.26;
+input.diag = 0; % flat to indicate whether pin moves through narrow (0) or wide (1) water
+% Pin information
+input.pinmats = [6, 2, 1; % Mixture
+    1, 2, 1; % Control Rod
+    5, 2, 1; % Guide Tube
+    4, 2, 1]; % Fuel
+
+input.radii = [ 0.4096, 0.475;
+    0.4, 0.475;
+    0.4, 0.475;
+    0.4, 0.475];
+input.pinmesh = [ 15 2 15;
+    15 2 15;
+    15 2 15;
+    15 2 15];
+% Mixtures
+input.nmixtures = 1;
+input.mixtures = [6, 1, 5];
+input.mixvols = [0.5, 0.5];
+% Quadrature
+input.npol = 32;
+% XS Library Info
+input.xsfilename = 'c5g7.xsl';
+input.scattype = 'P0';
+% Boundary Conditions
+input.BCond = ['vacuum';'vacuum'];
+% Convergence
+input.nouters = 2000;
+input.conv_crit = [1.0e-6 1.0e-6];
+input.verbose = true;
+
+%% Case 1 - 50-50 Mixutre Eigenvalue Case
+input.pinmap = [4, 1, 4];
