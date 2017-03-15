14bdb9d736c62841f2bd865cc95b4566a52c67ee
Aaron Graham <aarograh@umich.edu>
2017-03-14 22:08:08 -0400

    Updates to TenPin7group_mix_rodded_unrodded so it can be used with new scripts

--- 1D MOC/TenPin7group_mix_rodded_unrodded.m
+++ 1D MOC/TenPin7group_mix_rodded_unrodded.m
@@ -8,10 +8,10 @@ input = inputClass();
 input.pitch = 1.26;
 input.diag = 0; % flat to indicate whether pin moves through narrow (0) or wide (1) water
 % Pin information
-input.pinmats = [4, 2, 1; % Fuel
-    5, 2, 1; % Control Rod
-    1, 2, 1; % Guide Tube
-    6, 2, 1]; % 50-50 Mixture
+input.pinmats = [6, 2, 1; % Mixture
+    1, 2, 1; % Control Rod
+    5, 2, 1; % Guide Tube
+    4, 2, 1]; % Fuel
 
 input.radii = [ 0.4096, 0.475;
     0.4, 0.475;
@@ -35,30 +35,7 @@ input.BCond = ['vacuum';'vacuum'];
 % Convergence
 input.nouters = 2000;
 input.conv_crit = [1.0e-6 1.0e-6];
-input.verbose = false;
+input.verbose = true;
 
 %% Case 1 - 50-50 Mixutre Eigenvalue Case
-input.pinmap = [1, 4, 1, 1, 1, 1, 1, 1, 1, 1];
-% solver = MOC_1D(input);
-% names(1) = {sprintf('Mixed       - %g',solver(1).solution.keff(1))};
-% 
-% %% Case 2 - Control Rod Case
-% % pinmap_rodded = 1;
-% input.pinmap = [1, 2, 1, 1, 1, 1, 1, 1, 1, 1];
-% solver(2) = eigensolverClass(input);
-% % Copy source, angular flux
-% solver(2).solution.fisssrc(:) = solver(1).solution.fisssrc(:);
-% solver(2).solution.scalflux(:) = solver(1).solution.scalflux(:);
-% solver(2).solution.angflux(:) = solver(1).solution.angflux(:); 
-% solver(2).step(true);
-% names(2) = {sprintf('Rodded    - %g',solver(1).solution.keff(1))};
-% 
-% %% Case 3 - Guide Tube Case
-% input.pinmap = [1, 3, 1, 1, 1, 1, 1, 1, 1, 1];
-% solver(3) = eigensolverClass(input);
-% % Copy source, angular flux
-% solver(3).solution.fisssrc(:) = solver(1).solution.fisssrc(:);
-% solver(3).solution.scalflux(:) = solver(1).solution.scalflux(:);
-% solver(3).solution.angflux(:) = solver(1).solution.angflux(:);
-% solver(3).step(true);
-% names(3) = {sprintf('Unrodded - %g',solver(1).solution.keff(1))};
+input.pinmap = [4, 1, 4, 4, 4, 4, 4, 4, 4, 4];
