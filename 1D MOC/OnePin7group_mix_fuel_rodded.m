561f075912df844563d64758931da6082c27cfd2
Aaron Graham <aarograh@umich.edu>
2017-03-17 15:58:15 -0400

    Moving one things around in one pin subray test to be consistent with other inputs

--- 1D MOC/OnePin7group_mix_fuel_rodded.m
+++ 1D MOC/OnePin7group_mix_fuel_rodded.m
@@ -9,33 +9,35 @@ clc;
 input = inputClass();
 input.pitch = 1.26;
 input.diag = 0; % flat to indicate whether pin moves through narrow (0) or wide (1) water
-% Pin information
-input.pinmats = [6, 2, 1; % Mixture
-    4, 2, 1;  % Fuel
-    5, 2, 1]; % CR
-
-input.radii = [ 0.4096, 0.475;
-    0.4096, 0.475;
-    0.4096, 0.475];
-input.pinmesh = [ 15 2 15;
-    15 2 15;
-    15 2 15];
 % Mixtures
 input.nmixtures = 1;
 input.mixtures = [6, 4, 5];
 input.mixvols = [0.5, 0.5];
+% Pin information
+input.pinmats = [6, 2, 1; % Mixture
+    5, 2, 1;  % CR
+    4, 2, 1]; % Fuel
+
+input.radii = [ 0.4096, 0.475;
+    0.4096, 0.475;
+    0.4096, 0.475];
+input.pinmesh = [ 5 1 5;
+    5 1 5;
+    5 1 5];
 % Quadrature
-input.npol = 8;
+input.npol = 16;
 % XS Library Info
 input.xsfilename = 'c5g7.xsl';
 input.scattype = 'P0';
 % Boundary Conditions
 input.BCond = ['reflecting';'reflecting'];
 % Convergence
-input.nouters = 2000;
-input.conv_crit = [1.0e-6 1.0e-6];
+input.nouters = 500;
+input.conv_crit = [1.0e-5 1.0e-5];
 input.verbose = true;
-input.subray = false;
 
 %% Case 1 - 50-50 Mixutre Eigenvalue Case
 input.pinmap = [1];
+
+%% set IDs
+swappinids = [1,2,3];
