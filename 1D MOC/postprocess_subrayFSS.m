e0ce23a90b250e4ed5b2d75d372d49002273a47a
Aaron Graham <aarograh@umich.edu>
2017-03-17 14:51:17 -0400

    Changes to postprocess_subrayFSS.m to generate scalar flux plots for all groups instead of just the most thermal one.

--- 1D MOC/postprocess_subrayFSS.m
+++ 1D MOC/postprocess_subrayFSS.m
@@ -9,24 +9,25 @@ linS = {'-',':','--','-.'};
 
 
 % Scalar Flux Plots
-figure(1);
-hold on;
-igroup = fssSolver(1).xsLib.ngroups;
-cellcenter = 0.5*(fssSolver(1).mesh.fsredges(1:end-1) + fssSolver(1).mesh.fsredges(2:end));
-tmp = 0;
-for i=1:length(fssSolver)
-    plot(cellcenter,fssSolver(i).solution.scalflux(igroup,:,1),'linestyle',linS{i},'linewidth',2);
-    tmp = max(tmp,max(fssSolver(i).solution.scalflux(igroup,:,1)));
+for igroup=1:fssSolver(1).xsLib.ngroups
+	figure(igroup);
+	hold on;
+	cellcenter = 0.5*(fssSolver(1).mesh.fsredges(1:end-1) + fssSolver(1).mesh.fsredges(2:end));
+	tmp = 0;
+	for i=1:length(fssSolver)
+	    plot(cellcenter,fssSolver(i).solution.scalflux(igroup,:,1),'linestyle',linS{i},'linewidth',2);
+	    tmp = max(tmp,max(fssSolver(i).solution.scalflux(igroup,:,1)));
+	end
+	ax = gca;
+	ax.XAxis.TickValues = xgrid;
+	ax.XAxis.MinorTickValues = fssSolver(1).mesh.xsedges;
+	ax.XTickLabelRotation = 45;
+	ax.FontSize = 11;
+	xlabel('Position (cm)')
+	ylabel('Scalar Flux')
+	legend(names);
+	grid on;
+	grid minor;
 end
-ax = gca;
-ax.XAxis.TickValues = xgrid;
-ax.XAxis.MinorTickValues = fssSolver(1).mesh.xsedges;
-ax.XTickLabelRotation = 45;
-ax.FontSize = 11;
-xlabel('Position (cm)')
-ylabel('Scalar Flux')
-legend(names);
-grid on;
-grid minor;
 
 end
