3fc3cf095a2d31098b0b0db1a662cdbcd5a1cae0
Aaron Graham <aarograh@umich.edu>
2017-03-17 14:50:52 -0400

    Changes to compare_subrayFSS.m to use swappinids instead of assuming the pin ids to switch around

--- 1D MOC/compare_subrayFSS.m
+++ 1D MOC/compare_subrayFSS.m
@@ -1,6 +1,11 @@
+% This script assumes that the inputClass input has been defined
+% externally.  It also assumes that an array of pin IDs swappinids
+% has been set up.  The first index is the mixed pin, the second
+% the absorber pin, and the third the replacement pin
+
 %% Setup
 oldmap = input.pinmap;
-ninners = 1;
+ninners = 0;
 
 %% Eigensolve
 eSolver = eigensolverClass(input);
@@ -14,7 +19,7 @@ fssMixed.solve(0,ninners);
 % Fully rodded solve
 for i=1:length(input.pinmap)
     if oldmap(i) == 1
-        input.pinmap(i) = 3;
+        input.pinmap(i) = swappinids(2);
     end
 end
 fssCR = FixedSourceSolverClass(input, eSolver);
@@ -23,7 +28,7 @@ fssCR.solve(0,ninners);
 % Fully unrodded solve
 for i=1:length(input.pinmap)
     if oldmap(i) == 1
-        input.pinmap(i) = 2;
+        input.pinmap(i) = swappinids(3);
     end
 end
 fssNoCR = FixedSourceSolverClass(input, eSolver);
@@ -32,7 +37,7 @@ fssNoCR.solve(0,ninners);
 %% Sub-ray fixed source solve
 for i=1:length(input.pinmap)
     if oldmap(i) == 1
-        input.pinmap(i) = 1;
+        input.pinmap(i) = swappinids(1);
     end
 end
 input.subray = true;
@@ -44,5 +49,5 @@ fssSolver(1) = fssMixed;
 fssSolver(2) = fssNoCR;
 fssSolver(3) = fssCR;
 fssSolver(4) = fssSubray;
-names = {'Volume-Mixed','Unrodded','Rodded','Subray'};
+names = {'Volume-Mixed','Rodded','Unrodded','Subray'};
 postprocess_subrayFSS(input, fssSolver, names);
