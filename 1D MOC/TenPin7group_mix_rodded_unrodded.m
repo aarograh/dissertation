6bd4d176b638e4821cded71718fb7813b5b13485
Aaron Graham <aarograh@umich.edu>
2017-03-17 14:49:58 -0400

    Input changes to make three and ten pin test cases consistent

--- 1D MOC/TenPin7group_mix_rodded_unrodded.m
+++ 1D MOC/TenPin7group_mix_rodded_unrodded.m
@@ -7,35 +7,38 @@ close all; clear variables; clc;
 input = inputClass();
 input.pitch = 1.26;
 input.diag = 0; % flat to indicate whether pin moves through narrow (0) or wide (1) water
+% Mixtures
+input.nmixtures = 1;
+input.mixtures = [6, 1, 5];
+input.mixvols = [0.2, 0.8];
 % Pin information
 input.pinmats = [6, 2, 1; % Mixture
-    1, 2, 1; % Control Rod
-    5, 2, 1; % Guide Tube
+    5, 2, 1; % Control Rod
+    1, 2, 1; % Guide Tube
     4, 2, 1]; % Fuel
 
 input.radii = [ 0.4096, 0.475;
     0.4, 0.475;
     0.4, 0.475;
     0.4, 0.475];
-input.pinmesh = [ 15 2 15;
-    15 2 15;
-    15 2 15;
-    15 2 15];
-% Mixtures
-input.nmixtures = 1;
-input.mixtures = [6, 1, 5];
-input.mixvols = [0.5, 0.5];
+input.pinmesh = [ 5 1 5;
+    5 1 5;
+    5 1 5;
+    5 1 5];
 % Quadrature
-input.npol = 32;
+input.npol = 16;
 % XS Library Info
 input.xsfilename = 'c5g7.xsl';
 input.scattype = 'P0';
 % Boundary Conditions
-input.BCond = ['vacuum';'vacuum'];
+input.BCond = ['reflecting';'reflecting'];
 % Convergence
 input.nouters = 2000;
-input.conv_crit = [1.0e-6 1.0e-6];
+input.conv_crit = [1.0e-5 1.0e-5];
 input.verbose = true;
 
 %% Case 1 - 50-50 Mixutre Eigenvalue Case
-input.pinmap = [4, 1, 4, 4, 4, 4, 4, 4, 4, 4];
+input.pinmap = [4, 4, 1, 4, 1, 4, 1, 4, 4, 4];
+
+%% set IDs
+swappinids = [1,2,3];
--- 1D MOC/ThreePin7group_mix_rodded_unrodded.m
+++ 1D MOC/ThreePin7group_mix_rodded_unrodded.m
@@ -7,10 +7,14 @@ close all; clear variables; clc;
 input = inputClass();
 input.pitch = 1.26;
 input.diag = 0; % flat to indicate whether pin moves through narrow (0) or wide (1) water
+% Mixtures
+input.nmixtures = 1;
+input.mixtures = [6, 1, 5];
+input.mixvols = [0.2, 0.8];
 % Pin information
 input.pinmats = [6, 2, 1; % Mixture
-    1, 2, 1; % Control Rod
-    5, 2, 1; % Guide Tube
+    5, 2, 1; % Control Rod
+    1, 2, 1; % Guide Tube
     4, 2, 1]; % Fuel
 
 input.radii = [ 0.4096, 0.475;
@@ -21,12 +25,8 @@ input.pinmesh = [ 5 1 5;
     5 1 5;
     5 1 5;
     5 1 5];
-% Mixtures
-input.nmixtures = 1;
-input.mixtures = [6, 1, 5];
-input.mixvols = [0.5, 0.5];
 % Quadrature
-input.npol = 4;
+input.npol = 16;
 % XS Library Info
 input.xsfilename = 'c5g7.xsl';
 input.scattype = 'P0';
@@ -34,8 +34,11 @@ input.scattype = 'P0';
 input.BCond = ['reflecting';'reflecting'];
 % Convergence
 input.nouters = 2000;
-input.conv_crit = [1.0e-4 1.0e-4];
+input.conv_crit = [1.0e-5 1.0e-5];
 input.verbose = true;
 
 %% Case 1 - 50-50 Mixutre Eigenvalue Case
 input.pinmap = [4, 1, 4];
+
+%% set IDs
+swappinids = [1,2,3];
