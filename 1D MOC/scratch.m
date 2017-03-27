1e7cf88aba78f5f87e95f2bc95c2150a5817346a
Aaron Graham <aarograh@umich.edu>
2017-03-27 15:27:31 -0400

    Changes to scratch.m to account for multiple variations of subray

--- 1D MOC/scratch.m
+++ 1D MOC/scratch.m
@@ -1,3 +1,4 @@
+close all;
 ncases = length(solutions(1,:));
 
 for i=1:ncases+2
@@ -6,16 +7,22 @@ for i=1:ncases+2
 	if i == 1
 		vfrac(i) = 0.0;
 		keffs(1,i) = solutions(2,1).keff(1);
-		keffs(4,i) = keffs(1,i);
+		keffs(4:7,i) = keffs(1,i);
 	elseif i == ncases+2
 		vfrac(i) = 100.0;
 		keffs(1,i) = solutions(3,i-2).keff(1);
-		keffs(4,i) = keffs(1,i);
+		keffs(4:7,i) = keffs(1,i);
 	else
 		vfrac(i) = (i-1)/(ncases+1)*100;
 		keffs(1,i) = solutions(1,i-1).keff(1);
 		keffs(4,i) = solutions(4,i-1).keff(1);
+		keffs(5,i) = solutions(5,i-1).keff(1);
+		keffs(6,i) = solutions(6,i-1).keff(1);
+		keffs(7,i) = solutions(7,i-1).keff(1);
 	end
 end
 
-plot(vfrac,keffs(1,:),'k',vfrac,keffs(4,:),'r-.')
+plot(vfrac,keffs(1,:),'k',vfrac,keffs(4,:),'r--s',vfrac,keffs(5,:),'b:*',...
+    vfrac,keffs(6,:),'g-.+',vfrac,keffs(7,:),'k-o')
+legend('Volume Homogenization','Subray','Subray w/ Recombination - 0','Subray w/ Recombination - 1',...
+    'Subray w/ Recombination - 2')
