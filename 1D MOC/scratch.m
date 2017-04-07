da75d5bb84fa0211d378b51e728507e4bba42f1e
Aaron Graham <aarograh@umich.edu>
2017-04-07 15:12:04 -0400

    Changes to scratch.m to generate nicer plots

--- 1D MOC/scratch.m
+++ 1D MOC/scratch.m
@@ -22,7 +22,37 @@ for i=1:ncases+2
 	end
 end
 
-plot(vfrac,keffs(1,:),'k',vfrac,keffs(4,:),'r--s',vfrac,keffs(5,:),'b:*',...
-    vfrac,keffs(6,:),'g-.+',vfrac,keffs(7,:),'k-o')
-legend('Volume Homogenization','Subray','Subray w/ Recombination - 0','Subray w/ Recombination - 1',...
-    'Subray w/ Recombination - 2')
+
+
+isol = 5;
+xgrid = 0.5*(fssSolver(1).mesh.fsredges(2:end)+fssSolver(1).mesh.fsredges(1:end-1));
+for igroup=1:7
+    figure(igroup)
+    plot(xgrid,solutions(1,isol).scalflux(igroup,:,1),'k',xgrid,solutions(4,isol).scalflux(igroup,:,1),'r-s',...
+        xgrid,solutions(5,isol).scalflux(igroup,:,1),'b:*',xgrid,solutions(6,isol).scalflux(igroup,:,1),'g-.+',...
+        xgrid,solutions(7,isol).scalflux(igroup,:,1),'k--o','linewidth',2)
+    title(sprintf('Scalar Flux at 50%% Rod Withdrawal, Group %i',igroup))
+    legend('Volume Homog.','Subray w/o Recomb.','Subray w/ Recomb. - 0','Subray w/ Recomb. - 1',...
+        'Subray w/ Recomb. - 2')
+    ax = gca;
+    ax.XAxis.TickValues = min(fssSolver(1).mesh.xsedges):input.pitch:max(fssSolver(1).mesh.xsedges);
+    ax.XAxis.MinorTickValues = fssSolver(1).mesh.xsedges;
+    ax.XTickLabelRotation = 45;
+    ax.FontSize = 28;
+    xlabel('Position (cm)');
+    xlim([0,21.42]);
+    ylabel('Scalar Flux');
+    grid on;
+end
+figure(8)
+% plot(vfrac,keffs(1,:),'k',vfrac,keffs(4,:),'r--s',vfrac,keffs(5,:),'b:*')
+% legend('Volume-Homogenized XS','Volume-Homogenized Flux','Subray Calculation')
+plot(vfrac,keffs(1,:),'k',vfrac,keffs(4,:),'r-s',vfrac,keffs(5,:),'b:*',...
+    vfrac,keffs(6,:),'g-.+',vfrac,keffs(7,:),'k--o','linewidth',2)
+    legend('Volume Homog.','Subray w/o Recomb.','Subray w/ Recomb. - 0','Subray w/ Recomb. - 1',...
+        'Subray w/ Recomb. - 2')
+title('K-eff vs. Control Rod Withdrawal');
+ax = gca;
+ax.FontSize = 28;
+xlabel('Control Rod Percent Withdrawn');
+ylabel('k-eff');
